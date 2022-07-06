dfd14f06226af8bd85084ab00648b54b937e5b25
GreenDog <2891954521@qq.com>
2022-07-06 12:08:57 +0800

    Web端支持

--- Server/api.py
+++ Server/api.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 
 import Core
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/', static_folder='static')
 
 
 @app.route('/upload', methods = ['POST'])
